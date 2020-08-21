0e6c573f5ef260c24edaf2755e33921f61da338f
Mertkan <mertkansen@gmail.com>
2020-08-21 22:11:28 +0300

    lots of ui stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home/Home';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>Search Page</h1>
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+
+        </Switch>
+
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Home.css'
+
+import Search from '../Search/Search'
+
+//Icons
+import AppsIcon from '@material-ui/icons/Apps'
+import { Avatar } from '@material-ui/core'
+
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__header">
+                <div className="home__headerLeft">
+                    <Link to="/about">About</Link>
+                    <Link to="/store">Store</Link>
+                </div>
+                <div className="home__headerRight">
+                    <Link to="/gmail">Gmail</Link>
+                    <Link to="/images">Images</Link>
+                    <AppsIcon />
+                    <Avatar src="" />
+                </div>
+            </div>
+
+            <div className="home__body">
+                <img alt="" src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"/>
+                <div className="home_inputContainer">
+                    <Search hideButtons />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
